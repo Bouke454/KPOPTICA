abfb567d00f551f74bbf9f3be56e89afbb2b28d9
DESKTOP-TBI0NLQ\bouke <bouke.devries@student.hu.nl>
2018-12-30 18:41:48 +0100

    Prevent player to grab multiple items of the same type (inventory or while holding).

--- KPO-Optica/Assets/Scripts/Pickup.cs
+++ KPO-Optica/Assets/Scripts/Pickup.cs
@@ -7,6 +7,7 @@ public class Pickup : MonoBehaviour {
     private Inventory inventory;
     public GameObject itemButton;
     public GameObject effect;
+    private bool newItem;
 
     private void Start()
     {
@@ -14,8 +15,30 @@ public class Pickup : MonoBehaviour {
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) {
-            // spawn the sun button at the first available inventory slot ! 
+        //Prevent multiple items of the same type stored inside the inventory
+        foreach (GameObject slot in inventory.slots) {
+            var children2 = slot.GetComponentsInChildren<Transform>();
+            foreach (var child in children2) {
+
+                string CurrentItem = child.name;
+                if (child.name == itemButton.name + "(Clone)") {
+                    newItem = true;
+                }
+            }
+        }
+        //Prevent the item from being grabbed while holding the same item
+        string PickupItem = itemButton.name;
+        string Holdingitem = PickupItem.Replace("Button", "Wep");
+        var children3 = inventory.GetComponentsInChildren<Transform>();
+        foreach (var child2 in children3) {
+            if (child2.name == Holdingitem + "(Clone)") {
+                newItem = true;
+            }
+        }
+
+
+        if (other.CompareTag("Player") && newItem == false) {
+            // spawn the item button at the first available inventory slot ! 
             for (int i = 0; i < inventory.items.Length; i++)
             {
                 if (inventory.items[i] == 0) { // check whether the slot is EMPTY
@@ -26,7 +49,11 @@ public class Pickup : MonoBehaviour {
                     break;
                 }
             }
+        } 
+        else if (newItem == true){
+            newItem = false;
         }
+        
         
     }
 }
