c02f5ce67c393b78d14fc384a6d8c085f7c2d4ea
DESKTOP-TBI0NLQ\bouke <bouke.devries@student.hu.nl>
2019-01-03 15:40:20 +0100

    Lighting system improved -prism added -Improved lightning code

---
+++ KPO-Optica/Assets/Scripts/PrismaDetectLight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrismaDetectLight : MonoBehaviour {
+    public GameObject Prisma;
+    private LightingCollider2D controlLighting;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Flashlight")) {
+            controlLighting = gameObject.GetComponent<LightingCollider2D>();
+            controlLighting.enabled = true;
+            Prisma.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        Prisma.SetActive(false);
+        controlLighting = gameObject.GetComponent<LightingCollider2D>();
+        controlLighting.enabled = false;
+
+    }
+
+}
