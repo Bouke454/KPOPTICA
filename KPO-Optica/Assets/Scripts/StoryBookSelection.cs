7e00369502aafe7d06a4488265b56c35d38c2681
DESKTOP-TBI0NLQ\bouke <bouke.devries@student.hu.nl>
2019-01-23 17:56:29 +0100

    Added storybook and prisma interaction

---
+++ KPO-Optica/Assets/Scripts/StoryBookSelection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class StoryBookSelection : MonoBehaviour {
+
+    public GameStatus status;
+    public GameObject PageForward;
+    public GameObject CurrentPage;
+    public void BookNext() {
+        //krijg waarde van de button level0 dus 0
+        string GetButtonNumber = EventSystem.current.currentSelectedGameObject.name.Substring(5);
+        Debug.Log(GetButtonNumber + "HET BOEK");
+
+
+
+        //vergelijk waarde van volgende button en match dat met het nummer van de gamestatus
+        status.GrantLevelAcces(GetButtonNumber);
+        if (status.TravelPermission == true) {
+            status.TravelPermission = false;
+            PageForward.SetActive(true);
+            CurrentPage.SetActive(false);
+        } else {
+            Debug.Log("You have not unlocked this page yet");
+        }
+
+    }
+}
+
+
---
+++ KPO-Optica/Assets/Scripts/StoryBookSelectionReturn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class StoryBookSelectionReturn : MonoBehaviour {
+
+    public GameStatus status;
+    public GameObject PageBackward;
+    public GameObject CurrentPage;
+   
+    public void BookReturn() {
+        PageBackward.SetActive(true);
+        CurrentPage.SetActive(false);
+
+    }
+}
+
+
